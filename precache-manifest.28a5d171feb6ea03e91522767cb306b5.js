3347373d4c618b12e51e2b9f379eb15f25be99e3
none <none@gmail.com>
2020-08-31 09:55:17 +0100

    Updates

---
+++ precache-manifest.28a5d171feb6ea03e91522767cb306b5.js
@@ -0,0 +1,30 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "567359141c80b71299314814501bdcde",
+    "url": "/hbrwebclient/index.html"
+  },
+  {
+    "revision": "2389a2f4641605ce6760",
+    "url": "/hbrwebclient/static/css/main.5f361e03.chunk.css"
+  },
+  {
+    "revision": "c42b13b337a10c6d1960",
+    "url": "/hbrwebclient/static/js/2.8fe5b87c.chunk.js"
+  },
+  {
+    "revision": "e88a3e95b5364d46e95b35ae8c0dc27d",
+    "url": "/hbrwebclient/static/js/2.8fe5b87c.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "2389a2f4641605ce6760",
+    "url": "/hbrwebclient/static/js/main.646a0d53.chunk.js"
+  },
+  {
+    "revision": "9ce63a89cb033516827c",
+    "url": "/hbrwebclient/static/js/runtime-main.f810e880.js"
+  },
+  {
+    "revision": "5d5d9eefa31e5e13a6610d9fa7a283bb",
+    "url": "/hbrwebclient/static/media/logo.5d5d9eef.svg"
+  }
+]);
